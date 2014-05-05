8e2f083ddfccbdf8b22c7e036e9ec0aecadb70b5
Anbcorp <benoit@anbcorp.net>
2014-05-05 15:35:52 +0200

    Main bar driving xp gain

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ var clock = {
 
 setInterval('clock.tick()', clock.refresh);
 
+var toInt = function(num) {
+  return Math.round(Number(num));
+};
+
 module.service('Character', function($rootScope){
   this.level = 1;
   this.experience = 0;
@@ -30,6 +34,11 @@ module.service('Character', function($rootScope){
 
   this.addXp = function(xp) {
     this.experience += xp;
+
+
+    if(this.experience >= this.levelMaxXP()) {
+      this.levelup();
+    }
     $rootScope.$broadcast('CharXpChange');
   };
 
@@ -41,38 +50,45 @@ module.service('Character', function($rootScope){
 
 // We will probably need a CharacterController to display things like XP,
 // Health, Level
-var LevelCtrl = function($scope, Character) {
+var CharSheetCtrl = function($scope, Character) {
   $scope.level = Character.level;
+  $scope.lastMaxXp = 0;
+  $scope.xp = Character.experience;
+  $scope.xpPercent = toInt(((Character.experience - $scope.lastMaxXp)*100)/(Character.levelMaxXP()-$scope.lastMaxXp));
+  $scope.maxXp = Character.levelMaxXP();
 
-  $scope.$on('NewLevel', function(){
+  $scope.$on('NewLevel', function() {
+    $scope.lastMaxXp = $scope.maxXp;
+    $scope.maxXp = Character.levelMaxXP();
     $scope.level = Character.level;
+    // $scope.xp = Character.experience;
+    // $scope.xpPercent = Math.round(Number(((Character.experience - $scope.lastMaxXp)*100)/(Character.levelMaxXP()-$scope.lastMaxXp)));
     $scope.$apply();
+    console.log('New Level');
   });
 
   $scope.$on('CharXpChange', function(){
     $scope.xp = Character.experience;
-    $scope.maxXp = Character.levelMaxXP();
+    $scope.xpPercent = toInt(((Character.experience - $scope.lastMaxXp)*100)/(Character.levelMaxXP()-$scope.lastMaxXp));
+    console.log("XpChange "+$scope.xp);
     $scope.$apply();
   });
 };
 
 // TODO: This is the experience bar, we probably need a mainController to fire various events depending on the ticks and actions in progress
-var ProgressDemoCtrl = function ($scope, Character) {
+var MainBarCtrl = function ($scope, Character) {
   $scope.max = 100;
   $scope.progress = 0;
   $scope.lastLevelMax = 0;
 
 // TODO: change this to a reaction on the tick event.
   $scope.tick = function() {
-    Character.addXp(Character.level*10);
-    $scope.progress = Math.round(Number(((Character.experience - $scope.lastLevelMax)*100)/(Character.levelMaxXP()-$scope.lastLevelMax)));
-    
-    if(Character.experience > Character.levelMaxXP()) {
-      $scope.lastLevelMax = Character.levelMaxXP();
-      Character.levelup();
-      $scope.progress = Math.round(Number(((Character.experience - $scope.lastLevelMax)*100)/(Character.levelMaxXP()-$scope.lastLevelMax)));
+    $scope.progress += 50;
+
+    if($scope.progress > $scope.max) {
+      $scope.progress = 0;
+      Character.addXp(toInt(Character.level*50-20/Character.level));
     }
-    console.log($scope.progress+'/'+$scope.max+','+Character.level);
     // apply change to the view
     $scope.$apply();
   };
